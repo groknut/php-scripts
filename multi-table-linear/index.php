911d8a0b65f237081bd0ca8a7df567577bdc9d0d
groknut <184639205+groknut@users.noreply.github.com>
2026-03-12 12:30:43 +0800

    init multi-table-linear task

---
+++ multi-table-linear/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<head>
+    <title>Таблица умножения</title>
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+
+<table>
+    <?php
+    $size = 10;
+
+    for ($i = 1; $i <= $size; $i++) {
+        echo "<tr>";
+
+        for ($j = 1; $j <= $size; $j++) {
+            $res = $i * $j;
+            $class = '';
+            if ($i === 1 || $j === 1) $class = 'header';
+            elseif ($i === $j) $class = 'diagonal';
+            echo "<td class='$class'>$res</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
+
+</body>
+</html>
